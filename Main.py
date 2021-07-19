f01da04065dc37a5aff81f8df1fa47e86217f69c
Lea Vandamme <lea.vandamme.etu@univ-lille.fr>
2021-07-19 11:44:57 +0200

    modif argparse

--- Main.py
+++ Main.py
@@ -9,7 +9,7 @@ if __name__ == "__main__":
 
     parser = argparse.ArgumentParser()
     parser.add_argument("peptidesCSVFile", help="The name of the file containing peptides (CSV File)", type=str)
-    parser.add_argument("resultsPath", help="The directory (path) wich will contain all the result files (ex : results/), the path must end with a '/'", type=str)
+    parser.add_argument("resultsPath", help="The directory (path) wich will contain all the result files (ex : results/)", type=str)
     parser.add_argument("-p", "--peptideThreshold", help="The minimum length of the peptides we keep (default 1)", type=int, default=1)
     parser.add_argument("-c", "--combinationThreshold", help="The maximum size of a combination (default no limit)", type=int, default=0)
     args = parser.parse_args()
